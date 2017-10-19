b26636fbe6e24f69f605353189c4daed26904bcd
Igor Kotkovets <igorkotkovets@users.noreply.github.com>
2017-10-19 10:26:23 +0300

    Add some useful extensions for UI Tests

--- CleanTests/Classes/TestScreen.swift
+++ CleanTests/Classes/TestScreen.swift
@@ -10,18 +10,66 @@ import XCTest
 
 @available(iOS 9.0, *)
 open class TestScreen {
-    public let app: XCUIApplication
-
     @available(iOS 9.0, *)
-    public init(with app: XCUIApplication) {
+    public var app: XCUIApplication
+
+    @available(tvOS 10.0, *)
+    public var remoteControl: XCUIRemote {
+        return XCUIRemote()
+    }
+
+    public init(in app: XCUIApplication) {
         self.app = app
     }
 
-    open func waitForLoaded(timeout: TimeInterval) -> Bool {
+    open func waitForExistence(timeout: TimeInterval) -> Bool {
         return false
     }
 
-    open func startNewTest() {
+    open func launchApp() {
         app.launch()
+    }
+
+    open func launchWithResetApp() {
+        app.launchArguments = ["--Reset"]
+        app.launch()
+    }
+}
+
+public extension TestScreen {
+    func remoteSwipeRight(times count: Int = 1) {
+        for _ in 0..<count {
+            self.remoteControl.press(.right)
+        }
+    }
+
+    func remoteSwipeDown(times count: Int = 1) {
+        for _ in 0..<count {
+            self.remoteControl.press(.down)
+        }
+    }
+
+    func remoteSwipeLeft(times count: Int = 1) {
+        for _ in 0..<count {
+            self.remoteControl.press(.left)
+        }
+    }
+
+    func remoteSwipeUp(times count: Int = 1) {
+        for _ in 0..<count {
+            self.remoteControl.press(.up)
+        }
+    }
+
+    func remotePressSelect(times count: Int = 1) {
+        for _ in 0..<count {
+            self.remoteControl.press(.select)
+        }
+    }
+
+    func remotePressMenu(times count: Int = 1) {
+        for _ in 0..<count {
+            self.remoteControl.press(.menu)
+        }
     }
 }
---
+++ XCUIApplication+Extensions.swift
@@ -0,0 +1,10 @@
+//
+//  XCUIApplication+Extensions.swift
+//  Pods
+//
+//  Created by igork on 10/18/17.
+//
+
+import XCTest
+
+
